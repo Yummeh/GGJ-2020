c89b8eee163dc16ae16a9a45c7ade4bdec85ceca
LagMeester4000 <lagmeester4000@gmail.com>
2020-02-01 00:45:47 +0100

    Added AbilityManager, AbilityBase and PlayerInfo

---
+++ Test game/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public int health { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Called when player health reaches 0
+    void OnDeath()
+    {
+
+    }
+
+    // Deal damage to the player
+    public void DealDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            OnDeath();
+        }
+    }
+}

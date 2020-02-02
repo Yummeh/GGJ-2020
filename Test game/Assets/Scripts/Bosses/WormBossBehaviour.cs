a1e3c1e374f9c26bf1644fa9cb8c9a77949c8b43
Johan <JohannesMelis2000@gmail.com>
2020-02-02 11:22:13 +0100

    Added worm prefab (broken)

--- Test game/Assets/Scripts/Bosses/WormBossBehaviour.cs
+++ Test game/Assets/Scripts/Bosses/WormBossBehaviour.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class WormBossBehaviour : BaseBossBehaviour
 {
-    void Start()
+    [SerializeField] private GameObject wormPartPrefab;
+    [SerializeField] private Transform wormPartParent;
+    private List<WormPart> wormParts = new List<WormPart>();
+
+
+    public Transform connectionPoint;
+
+    protected override void Start()
     {
-        
+        base.Start();
+
+        Transform prevTrans = connectionPoint;
+        for(int i = 0; i < defaultHealth; i++)
+        {
+            wormParts.Add(Instantiate(wormPartPrefab, wormPartParent).GetComponent<WormPart>());
+
+            Vector3 Diameter = (wormParts[i].connectionPointEnd.position - wormParts[i].connectionPointFront.position);
+            transform.position = prevTrans.position - Diameter / 2;
+
+            wormParts[i].connectedToTransform = prevTrans;
+            prevTrans = wormParts[i].connectionPointEnd;
+        }
     }
 
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+
+
     }
 }
